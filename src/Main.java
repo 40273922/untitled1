446c7651b671f9785c18a143140046e17c4c785e
“goodboy” <88391769+40273922@users.noreply.github.com>
2022-11-01 00:44:04 +0800

    no

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,10 @@
  * &#064;create: -10-05 10:48
  **/
 public class Main {
-
+    public static void main(String[] args) {
+        g("Hello");
+    }
+    public static void g (String i){
+        System.out.println(i);
+    }
 }
